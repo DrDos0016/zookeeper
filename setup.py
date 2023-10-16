2b114adc90842897cf99e0b47621da731d894cf6
Adrian Siekierka <kontakt@asie.pl>
2023-10-16 18:55:52 +0200

    Fix #13, adjust stat drawing to be more in line with ZZT 3.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     url="https://github.com/DrDos0016/zookeeper",
     license="GPL 3",
     description="A Python library for parsing, analyzing, and modifying ZZT worlds.",
-    long_description=open('README', 'r').read(),
+    long_description=open('README.md', 'r').read(),
     packages=["zookeeper",],
     package_data={
         "zookeeper": ["charsets/*.png", "palettes/*.dat"],
